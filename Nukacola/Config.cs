c8d77631ef3daa63d74a369d3331ef28d33a52e7
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-01-15 17:39:48 +0100

    All can be configurable now

---
+++ Nukacola/Config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace Nukacola
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [Description("Item Config")]
+        public float SpawnChance { get; set; } = 50;
+        public float MovementBoostEffectDuration { get; set; } = 80;
+        public float VitalityEffectDuration { get; set; } = 20;
+        public float HintsDuration { get; set; } = 5;
+    }
+}
